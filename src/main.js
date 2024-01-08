6dc2fa3a007dafbade3814ec394bd5dd806e1ff6
Joselon <jose.navarro.osta@gmail.com>
2024-01-08 19:27:09 +0000

    Clase 3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import './components/dw-message.js';
-import './styles.css';
 import '@dile/dile-nav/dile-nav.js';
 import '@dile/dile-tabs/dile-tabs.js';
 import '@dile/dile-pages/dile-pages.js';
 import '@dile/dile-menu-hamburger/dile-menu-hamburger.js';
+import '@dile/dile-selector/dile-selector.js';
+import '@dile/dile-selector/dile-selector-item.js'; 
+import './components/dw-message.js';
+import './styles.css';
+
+document.getElementById("selector").addEventListener('dile-selected-changed', function() {
+    document.querySelector('dile-menu-hamburger').close();
+    document.querySelector('dile-tabs').selected = document.getElementById("selector").selected;
+})
